f9abc179b21a5ca9a3ba649c0d514febfad38461
Vinay9979 <rathodvinay34@gmail.com>
2025-01-21 17:49:55 +0530

    changes

--- mypro/api/views.py
+++ mypro/api/views.py
@@ -13,27 +13,9 @@ from rest_framework.permissions import IsAuthenticated
 from django.db.models import F,Sum
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.exceptions import NotFound
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from rest_framework.viewsets import ModelViewSet
 
-# class CartApi(APIView):
-#     # permission_classes =  [IsAuthenticated]
-   
-#     def get_queryset(self):
-#         queryset = Cart.objects.all()
-#         return queryset
-    
-#     def get(self,request):
-#         cart_items = self.get_queryset()
-#         total = cart_items.aggregate(total = Sum(F('product__purchasePrice'))) #filter(user = self.request.user)
-#         total = total['total']
-#         if cart_items.exists():
-#             serializer  = serializers.CartSerializer(cart_items,total, many=True)
-#             return Response(serializer.data)
-#         else:
-#             raise NotFound("No cart items found")
         
 class TokenApi(APIView):
 
@@ -70,11 +52,6 @@ class CartApi(APIView):
             }
         )
 
-# class CategroryDetail(ModelViewSet):
-#     queryset = Category.objects.all()
-#     serializer_class  = serializers.CategorySerializer
-#     def get_serializer_context(self):
-#         return {'cart_items'}
 
 
 
